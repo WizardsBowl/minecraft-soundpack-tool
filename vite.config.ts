a4d404815fc75c5d648132c88aea2a4cd02e977f
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-09-16 22:38:46 +0800

    更改dev端口为5203，添加vscode启动配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5203,
+  },
   plugins: [vue(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
